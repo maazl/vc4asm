cdc2264c6ad2574fbe8dd40b23c9ff68c1b48e4b
Marcel Müller <github@maazl.de>
2015-04-06 22:28:23 +0000

    Fix: convertion to mov instruction

--- src/Disassembler.cpp
+++ src/Disassembler.cpp
@@ -89,8 +89,8 @@ void Disassembler::DoADD()
 
 	if ( UseMOV
 		&& ( (Instruct.MuxAA == Instruct.MuxAB && (0x807c2000 & (1<<opa))) // Both inputs equal and instruction that returns identity or 0
-			|| ( Instruct.Sig == Inst::S_SMI &&
-					(Instruct.MuxAA == Inst::X_RB || (Instruct.MuxAB == Inst::X_RB && isUnary)) ))) // unary or binary operator on constant
+			|| ( Instruct.Sig == Inst::S_SMI && Instruct.MuxAB == Inst::X_RB
+				&& (isUnary || Instruct.MuxAA == Inst::X_RA) ))) // unary or binary operator on constant
 		opa = 32;
 
 	append(cOpA[opa]);
